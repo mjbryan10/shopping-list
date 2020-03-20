8037f03e0bc96fc72ca0b06a2e55f73088bb5d94
mjbryan <mjbyran10@gmail.com>
2020-03-20 15:18:40 +0100

    list and delete

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
+// import 'react-native-get-random-values';
+import uuid from 'react-native-uuid';
 
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid.v4(), text: 'Milk'},
+    {id: uuid.v4(), text: 'Bread'},
+    {id: uuid.v4(), text: 'Eggs'},
+    {id: uuid.v4(), text: 'Juice'},
+  ]);
+
+  const deleteItem = (id: string): any => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
